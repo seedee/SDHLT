37018819c4c0c7510f401cdb7b8813f72db2fc17
seedee <cdaniel9000@gmail.com>
2022-08-27 06:19:11 +0100

    Create cleanup.bat

---
+++ src/cleanup.bat
@@ -0,0 +1,16 @@
+::This script is used to delete any Release folders that are created after compilation
+
+rmdir /s /q sdhlt\sdHLBSP\Release\
+rmdir /s /q sdhlt\sdHLBSP\Release_x64\
+
+rmdir /s /q sdhlt\sdHLCSG\Release\
+rmdir /s /q sdhlt\sdHLCSG\Release_x64\
+
+rmdir /s /q sdhlt\sdHLRAD\Release\
+rmdir /s /q sdhlt\sdHLRAD\Release_x64\
+
+rmdir /s /q sdhlt\sdHLVIS\Release\
+rmdir /s /q sdhlt\sdHLVIS\Release_x64\
+
+rmdir /s /q sdhlt\sdRIPENT\Release\
+rmdir /s /q sdhlt\sdRIPENT\Release_x64\
