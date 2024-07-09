0094acb9fb0c926c175f4a902fb1cfa30d3249b9
seedee <cdaniel9000@gmail.com>
2024-07-09 21:26:06 +0100

    Add comment

--- src/sdhlt/sdHLCSG/wadpath.h
+++ src/sdhlt/sdHLCSG/wadpath.h
@@ -9,9 +9,9 @@ typedef struct
 {
     char            path[_MAX_PATH];
     bool            usedbymap;        // does this map requrie this wad to be included in the bsp?
-    int             usedtextures;         // number of textures in this wad the map actually uses
-	int             totaltextures;
-} wadpath_t;//!!! the above two are VERY DIFFERENT. ie (usedtextures == 0) != (usedbymap == false)
+    int             usedtextures;     // number of textures in this wad the map actually uses
+	int             totaltextures;    // total textures in this wad
+} wadpath_t;                          // !!! the above two are VERY DIFFERENT. ie (usedtextures == 0) != (usedbymap == false)
 
 extern wadpath_t*  g_pWadPaths[MAX_WADPATHS];
 extern int         g_iNumWadPaths;    
