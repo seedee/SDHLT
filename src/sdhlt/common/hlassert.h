b6489ff1da27c48305ab48ebbd1378c7d06893bb
seedee <cdaniel9000@gmail.com>
2026-08-16 06:10:25 +0100

    Fix debug mode x64 compatibility

--- src/sdhlt/common/hlassert.h
+++ src/sdhlt/common/hlassert.h
@@ -11,7 +11,7 @@
 
 #include "log.h"
 
-#define assume(exp, message) {if (!(exp)) {Log("\n***** ERROR *****\nAssume '%s' failed\n at %s:%d\n %s\n\n", #exp, __FILE__, __LINE__, message);  __asm{int 3} }}
+#define assume(exp, message) {if (!(exp)) {Log("\n***** ERROR *****\nAssume '%s' failed\n at %s:%d\n %s\n\n", #exp, __FILE__, __LINE__, message);  __debugbreak(); }}
 #define hlassert(exp) assume(exp, "")
 
 #else // _DEBUG
