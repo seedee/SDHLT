023efc2d03560f659102db34bd7cb85cc4c6f374
seedee <cdaniel9000@gmail.com>
2026-08-20 19:27:22 +0100

    Format ambiguous leafnode warning for J.A.C.K.

--- src/sdhlt/sdHLBSP/solidbsp.cpp
+++ src/sdhlt/sdHLBSP/solidbsp.cpp
@@ -1208,7 +1208,8 @@ static void     LinkLeafFaces(surface_t* planelist, node_t* leafnode)
 		{
 			ent = NULL;
 		}
-		Warning ("Ambiguous leafnode content ( %s and %s ) at (%.0f,%.0f,%.0f)-(%.0f,%.0f,%.0f) in hull %d of model %d (entity: classname \"%s\", origin \"%s\", targetname \"%s\")", 
+		//Replace commas with spaces -FIXXOR
+		Warning ("Ambiguous leafnode content ( %s and %s ) at (%.0f %.0f %.0f)-(%.0f %.0f %.0f) in hull %d of model %d (entity: classname \"%s\", origin \"%s\", targetname \"%s\")", 
 			ContentsToString (ContentsForRank(r)), ContentsToString (ContentsForRank(rank)), 
 			leafnode->mins[0], leafnode->mins[1], leafnode->mins[2], leafnode->maxs[0], leafnode->maxs[1], leafnode->maxs[2], 
 			g_hullnum, g_nummodels - 1, 
